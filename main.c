000dba9f89a519cd237239bca020901ceaf37a86
Ademola Asamu <Ademola.Asamu@gmail.com>
2023-05-24 13:44:29 -0700

    iterative

--- main.c
+++ main.c
@@ -10,9 +10,10 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 	{
 		printf("Usage: %s [file]\n", argv[0]);
+		return (1);
 	}
 
-	if (isatty(STDIN_FILENO))
+	if (isatty(STDIN_FILENO) != 1)
 	{
 		handleInteractiveMode(argv);
 	}
